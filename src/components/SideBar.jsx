1fad866fef1a67c76f3f31669db1951eb3ae4dce
Nguyen To Duc Tai <nguyentoductai@gmail.com>
2024-10-14 17:06:40 +0700

    continute

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,16 @@
+import IconPlus from '../assets/plusIcon.png'
 
 const SideBar = () => {
-  return (
-    <div className="bg-primaryBg-sideBar w-[280px] h-screen">SideBar</div>
-  )
-}
+    return (
+        <div className="bg-primaryBg-sideBar w-[280px] h-screen text-white p-4">
+            <div className="mt-20">
+                <button className="px-4 py-1 flex items-center space-x-4 bg-gray-600">
+                    <img src={IconPlus} alt="Plus icon" className="w-4 h-4" />
+                    <p>Cuộc trò chuyện mới</p>
+                </button>
+            </div>
+        </div>
+    );
+};
 
-export default SideBar
+export default SideBar;
--- src/pages/ChatDetail.jsx
+++ src/pages/ChatDetail.jsx
@@ -1,10 +1,7 @@
-import IconPlus
 const ChatDetail = () => {
   return (
     <div className="text-white">
-        <div className="mt-20">
-            <button className="px-4 py-1 flex items-center space-x-4"></button>
-        </div>
+        
     </div>
   )
 }
