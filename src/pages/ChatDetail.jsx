1da514560fbf3c13582c25623b3d3c3b1c2ad20d
Nguyen To Duc Tai <nguyentoductai@gmail.com>
2024-10-14 16:55:33 +0700

    first commit

---
+++ src/pages/ChatDetail.jsx
@@ -0,0 +1,12 @@
+import IconPlus
+const ChatDetail = () => {
+  return (
+    <div className="text-white">
+        <div className="mt-20">
+            <button className="px-4 py-1 flex items-center space-x-4"></button>
+        </div>
+    </div>
+  )
+}
+
+export default ChatDetail
