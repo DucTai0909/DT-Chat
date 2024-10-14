e79967969a365d080c70a635c72a223646ddc3d3
Tài Nguyễn Tô Đức <nguyentoductai@gmail.com>
2024-10-14 23:21:21 +0700

    duoi

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,13 +1,35 @@
-import IconPlus from '../assets/plusIcon.png'
+import IconPlus from "../assets/plusIcon.png";
+import IconChat from "../assets/chat.png";
+import IconTrash from "../assets/remove.png";
 
 const SideBar = () => {
     return (
-        <div className="bg-primaryBg-sideBar w-[280px] h-screen text-white p-4">
+        <div className="bg-primaryBg-sideBar w-[280px] h-screen text-white p-8">
             <div className="mt-20">
-                <button className="px-4 py-1 flex items-center space-x-4 bg-gray-600">
+                <button className="px-4 py-1 flex items-center space-x-4 bg-gray-600 mb-10">
                     <img src={IconPlus} alt="Plus icon" className="w-4 h-4" />
                     <p>Cuộc trò chuyện mới</p>
                 </button>
+                <div className="space-y-4">
+                    <p>Gần đây:</p>
+                    <div className="flex flex-col space-y-6">
+                        <div className="flex items-center justify-between p-4 bg-gray-800">
+                            <div className="flex items-center space-x-4">
+                                <img
+                                    src={IconChat}
+                                    alt="Chat icon"
+                                    className="w-8 h-8"
+                                />
+                                <p>Chat</p>
+                            </div>
+                            <img
+                                src={IconTrash}
+                                alt="Chat icon"
+                                className="w-5 h-5"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/pages/ChatDetail.jsx
+++ src/pages/ChatDetail.jsx
@@ -1,9 +1,27 @@
 const ChatDetail = () => {
-  return (
-    <div className="text-white">
-        
-    </div>
-  )
-}
+    return (
+        <div className="text-white w-[80%]">
+            <h1 className="text-xl uppercase font-bold">Gemini</h1>
+            <div className="max-w-[70%] w-full mx-auto mt-32">
+                <div className="flex flex-col space-y-5">
+                    <div className="space-y-1">
+                        <h2
+                            className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 
+                    text-3xl inline-block text-transparent bg-clip-text text-[40px] font-bold"
+                        >
+                            Xin Chào
+                        </h2>
+                        <p className="text-3xl">
+                            Hôm nay tôi có thể giúp gì cho bạn
+                        </p>
+                    </div>
+                    <div className="flex items-center space-x-3">
+                        <div className="w-[200px] h-[200px] bg-primaryBg-sideBar"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default ChatDetail
+export default ChatDetail;
